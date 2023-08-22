a642d16ac7299f3bf44abe7785b3e80105d578b7
mraf19 <raflimr1902@gmail.com>
2023-08-22 09:59:03 +0700

    create chart

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,200 +4,335 @@ import { Chart } from 'angular-highcharts';
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
-  styleUrls: ['./main-content.component.scss']
+  styleUrls: ['./main-content.component.scss'],
 })
 export class MainContentComponent {
-
-  linechart = new Chart({
+  MTTRchart = new Chart({
     chart: {
       type: 'line',
-      backgroundColor: "black",
-      borderColor: 'white'
+      backgroundColor: 'black',
+      borderColor: 'white',
     },
     yAxis: {
       title: {
-        text: ""
+        text: '',
       },
       max: 100,
-      tickColor: "white",
-      color: "white"
+      tickColor: 'white',
+      color: 'white',
     },
-    colors: ["green", "yellow"],
+    colors: ['green', 'yellow'],
     xAxis: {
-      lineColor: "#fff",
-      max: 30
+      lineColor: '#fff',
+      max: 30,
     },
     title: {
       text: 'MTTR',
       align: 'left',
       style: {
-        color: "white",
-        fontSize: "12px"
-      }
+        color: 'white',
+        fontSize: '12px',
+      },
     },
     credits: {
-      enabled: false
+      enabled: false,
     },
     legend: {
-      enabled: false
-    } ,
+      enabled: false,
+    },
     series: [
       {
         name: 'MTTR',
-        data: [75, 76, 77, 78, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 75, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+        data: [
+          75, 76, 77, 78, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 75, 77,
+          78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+        ],
       } as any,
       {
         name: 'Plan',
-        data: [ 55, 56, 57, 58, 59, 60, 61, 62, 63, 65, 66, 67, 68, 69, 70, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70]
+        data: [
+          55, 56, 57, 58, 59, 60, 61, 62, 63, 65, 66, 67, 68, 69, 70, 56, 57,
+          58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+        ],
       } as any,
-      
-    ]
+    ],
   });
-  linechart2 = new Chart({
+  MTBFchart = new Chart({
     chart: {
       type: 'line',
-      backgroundColor: "black",
+      backgroundColor: 'black',
       borderColor: 'white',
     },
     yAxis: {
       title: {
-        text: "",
+        text: '',
       },
       max: 100,
-      tickColor: "white",
-      color: "white"
+      tickColor: 'white',
+      color: 'white',
     },
-    colors: ["green", "yellow"],
+    colors: ['green', 'yellow'],
     xAxis: {
-      lineColor: "#fff"
+      lineColor: '#fff',
     },
     title: {
       text: 'MTBF',
       align: 'left',
       style: {
-        color: "white",
-        fontSize: "12px"
-      }
+        color: 'white',
+        fontSize: '12px',
+      },
     },
     credits: {
-      enabled: false
+      enabled: false,
     },
     legend: {
       enabled: false,
       itemStyle: {
-        fontSize: "8px"
-      }
-    } ,
+        fontSize: '8px',
+      },
+    },
     series: [
-      
-        {
-          name: 'MTTf',
-          data: [75, 76, 77, 78, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 75, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
-        } as any,
-        {
-          name: 'Plan',
-          data: [ 55, 56, 57, 58, 59, 60, 61, 62, 63, 65, 66, 67, 68, 69, 70, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70]
-        } as any,
-        
-      
-    ]
+      {
+        name: 'MTTf',
+        data: [
+          75, 76, 77, 78, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 75, 77,
+          78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+        ],
+      } as any,
+      {
+        name: 'Plan',
+        data: [
+          55, 56, 57, 58, 59, 60, 61, 62, 63, 65, 66, 67, 68, 69, 70, 56, 57,
+          58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+        ],
+      } as any,
+    ],
   });
 
-  piechart = new Chart({
+  timechart = new Chart({
     chart: {
       plotBackgroundColor: '',
       plotBorderWidth: 0,
       plotShadow: false,
       type: 'pie',
-      backgroundColor: "black",
-      borderColor: "black"
+      backgroundColor: 'black',
+      borderColor: 'black',
     },
     tooltip: {
-        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
     },
     accessibility: {
-        point: {
-            valueSuffix: '%'
-        }
+      point: {
+        valueSuffix: '%',
+      },
     },
     title: {
-        text: ""
+      text: '',
     },
     plotOptions: {
-        pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-                enabled: false
-            },
-            showInLegend: true
-        }
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false,
+        },
+        showInLegend: true,
+      },
     },
     credits: {
-      enabled: false
+      enabled: false,
     },
-    colors: ["green", "yellow", "red"],
-    series: [{
+    colors: ['green', 'yellow', 'red'],
+    series: [
+      {
         name: 'Brands',
         colorByPoint: true,
-        data: [{
+        data: [
+          {
             name: 'Running',
             y: 20,
-        } as any,  {
+          } as any,
+          {
             name: 'Idle',
-            y: 20
-        } as any, {
+            y: 20,
+          } as any,
+          {
             name: 'Stop Line',
-            y: 30
-        }]
-    } as any]
-    })
+            y: 30,
+          },
+        ],
+      } as any,
+    ],
+  });
 
   voltageChart = new Chart({
     chart: {
       type: 'spline',
-      backgroundColor: "black"
+      backgroundColor: 'black',
     },
     title: {
-        text: ''
+      text: '',
     },
     subtitle: {
-        text: ''
+      text: '',
     },
     xAxis: {
-        categories: ["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      categories: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
     },
     yAxis: {
-        max: 100,
-        maxColor: "red",
-        title: {
-          text: ""
-        }
+      max: 100,
+      maxColor: 'red',
+      title: {
+        text: '',
+      },
     },
     credits: {
-      enabled: false
+      enabled: false,
     },
     tooltip: {
-        shared: true
+      shared: true,
     },
     plotOptions: {
-        spline: {
-            marker: {
-                radius: 4,
-                lineColor: '#666666',
-                lineWidth: 1
-            }
-        }
+      spline: {
+        marker: {
+          radius: 4,
+          lineColor: '#666666',
+          lineWidth: 1,
+        },
+      },
     },
-    colors: ["steelblue", "red"],
-    series: [{
+    colors: ['steelblue', 'red'],
+    series: [
+      {
         name: 'Actual',
-        data: [25, 5, 60, 30, 45, 20, 80, 40, 45, 30, 90]
+        data: [25, 5, 60, 30, 45, 20, 80, 40, 45, 30, 90],
+      },
+      {
+        name: 'Maximum',
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+      },
+    ] as any,
+  });
 
-    }, {
-      name: 'Maximum',
-      data: [100, 100, 100, 100, 100, 100, 100 ,100, 100, 100, 100]
+  ampereChart = new Chart({
+    chart: {
+      type: 'spline',
+      backgroundColor: 'black',
+    },
+    title: {
+      text: '',
+    },
+    subtitle: {
+      text: '',
+    },
+    xAxis: {
+      categories: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+    },
+    yAxis: {
+      max: 100,
+      maxColor: 'red',
+      title: {
+        text: '',
+      },
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      shared: true,
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          radius: 4,
+          lineColor: '#666666',
+          lineWidth: 1,
+        },
+      },
+    },
+    colors: ['steelblue', 'red'],
+    series: [
+      {
+        name: 'Actual',
+        data: [25, 5, 60, 30, 45, 20, 80, 40, 45, 30, 90],
+      },
+      {
+        name: 'Maximum',
+        data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+      },
+    ] as any,
+  });
 
-    }] as any
-  }) 
+  frequentChart = new Chart({
+    chart: {
+      type: 'bar',
+      backgroundColor: 'black',
+    },
+    credits: {
+      enabled: false,
+    },
+    title: {
+      text: '',
+    },
+    xAxis: {
+      categories: ['FR1', 'RR2', 'LSR', 'GA', 'PDHL'],
+    },
+    yAxis: {
+      title: {
+        text: '',
+      },
+      gridLineColor: 'black',
+    },
+    legend: {
+      enabled: false,
+    },
+    plotOptions: {
+      series: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+        },
+      },
+    },
+    colors: ['red'],
+    series: [
+      {
+        name: '',
+        data: [8, 6, 5, 4, 3],
+      },
+    ] as any,
+  });
+
+  productionChart = new Chart({
+    chart: {
+      type: 'column',
+      backgroundColor: 'black',
+    },
+    title: {
+      text: '',
+    },
+    xAxis: {
+      type: 'category',
+      categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      allowDecimals: false,
+      max: 100,
+    },
+    colors: ['steelblue', 'green'],
+    credits: {
+      enabled: false,
+    },
+    series: [
+      {
+        name: 'plan',
+        data: [70, 80, 60, 68, 62, 73, 60],
+      },
+      {
+        name: 'actual',
+        data: [71, 69, 43, 58, 44, 71, 53],
+      },
+    ] as any,
+  });
 }
